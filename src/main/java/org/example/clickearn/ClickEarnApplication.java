a6f2387eccda68fc7d2616af1424a238d72e8205
tawba-ayed <tawbaayed46@gmail.com>
2025-09-29 10:46:14 +0100

    Premier commit - Spring Boot project

---
+++ src/main/java/org/example/clickearn/ClickEarnApplication.java
@@ -0,0 +1,13 @@
+package org.example.clickearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickEarnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClickEarnApplication.class, args);
+    }
+
+}
